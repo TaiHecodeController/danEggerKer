00c89639282af24ef934f410e51859e65dc49ece
刘宇 <646251931@qq.com>
2015-09-04 10:14:31 +0800

    - 优化职位详情列表
    - 优化职位详情列表

--- EggShell/ViewControllers/Main/FindJob/JobDetails/moreJobCell/moreJobTableViewCell.h
+++ EggShell/ViewControllers/Main/FindJob/JobDetails/moreJobCell/moreJobTableViewCell.h
@@ -9,5 +9,12 @@
 #import <UIKit/UIKit.h>
 
 @interface moreJobTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *positionName;
+@property (weak, nonatomic) IBOutlet UILabel *time;
 
+@property (weak, nonatomic) IBOutlet UILabel *companyName;
+@property (weak, nonatomic) IBOutlet UILabel *addres;
+@property (weak, nonatomic) IBOutlet UILabel *knowdelge;
+
+@property (weak, nonatomic) IBOutlet UILabel *salary;
 @end
