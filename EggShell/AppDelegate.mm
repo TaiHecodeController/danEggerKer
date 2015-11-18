034e17985257e7fc531976a5b3bbfabae5669e6f
刘宇 <646251931@qq.com>
2015-11-18 11:41:24 +0800

    badge清零

--- EggShell/AppDelegate.mm
+++ EggShell/AppDelegate.mm
@@ -247,6 +247,7 @@
 }
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+     [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
