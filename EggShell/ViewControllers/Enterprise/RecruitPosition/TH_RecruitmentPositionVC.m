c25a05051c8b9ab55bbb17c51780ad0f1ff09265
lxj <lixianjun0903@163.com>
2015-11-26 11:45:44 +0800

    ss
    ss

--- EggShell/ViewControllers/Enterprise/RecruitPosition/TH_RecruitmentPositionVC.m
+++ EggShell/ViewControllers/Enterprise/RecruitPosition/TH_RecruitmentPositionVC.m
@@ -15,6 +15,11 @@
 @interface TH_RecruitmentPositionVC ()<UITableViewDataSource,UITableViewDelegate,MJRefreshBaseViewDelegate>
 {
     MBProgressHUD * _mbPro;
+    //时间选择
+    NSString * dateStr;
+    UIButton * record_btn;
+    UIView * backView;
+    UIButton * ok;
 }
 @property(nonatomic,strong)UITableView  * tableView;
 @property (nonatomic,strong)AFRequestState * state;
@@ -23,8 +28,10 @@
 @property(nonatomic,strong)MJRefreshHeaderView * header;
 @property(nonatomic,assign)int page;
 @property(nonatomic,assign)int limitNum;
+@property(nonatomic,copy)NSString * extensionDateStr;
+//时间选择
+@property (strong,nonatomic)UIDatePicker * dataPicker;
 @end
-
 @implementation TH_RecruitmentPositionVC
 -(void)dealloc
 {
@@ -92,8 +99,90 @@ for (int i = 0 ; i < titileArray.count; i++) {
     electBtn.layer.cornerRadius = 3;
     electBtn.layer.masksToBounds = YES;
     electBtn.backgroundColor = UIColorFromRGB(0xF44336);
+    electBtn.tag = i;
+    [electBtn addTarget:self action:@selector(electBtn:) forControlEvents:UIControlEventTouchUpInside];
   }
 }
+#pragma mark 职位延期删除
+-(void)electBtn:(UIButton*)sender
+{
+    if (sender.tag == 0) {
+        record_btn = sender;
+         [self createDataPickView];
+    }if (sender.tag == 1) {
+        
+    }
+}
+#pragma mark   延期时间选择
+-(void)createDataPickView
+{
+    
+    backView = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    backView.backgroundColor = [UIColor blackColor];
+    backView.alpha = 0.3;
+    [[AppDelegate instance].window addSubview:backView];
+    //创建picker
+    self.dataPicker = [[UIDatePicker alloc] initWithFrame:CGRectMake(0, HEIGHT, WIDETH, HEIGHT / 2 - 80)];
+    self.dataPicker.backgroundColor = [UIColor groupTableViewBackgroundColor];
+    [[AppDelegate instance].window addSubview:self.dataPicker];
+    [self.dataPicker addTarget:self action:@selector(dataChanged:) forControlEvents:UIControlEventValueChanged];
+    NSLocale * locale = [[NSLocale alloc] initWithLocaleIdentifier:@"zh_CN"];//设置显示为中文
+    self.dataPicker.locale = locale;
+    self.dataPicker.datePickerMode = UIDatePickerModeDate;
+    //当前时间创建NSDate
+    NSDate * localDate = [NSDate date];
+    //设置时间
+    NSDateFormatter * dateFormatt = [[NSDateFormatter alloc] init];
+    [dateFormatt setDateFormat:@"yyyy-MM-dd"];
+    NSDate * minDate = [dateFormatt dateFromString:@"1910-1-1"];
+    
+    //设置属性
+    self.dataPicker.maximumDate = localDate;
+    self.dataPicker.minimumDate = minDate;
+    
+    ok = [ZCControl createButtonWithFrame:CGRectMake(WIDETH - 40, HEIGHT, 40, 30) ImageName:@"" Target:self Action:@selector(okClick) Title:@"确定"];
+    self.dataPicker.userInteractionEnabled = YES;
+    [ok setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [[AppDelegate instance].window addSubview:ok];
+    
+    [UIView animateWithDuration:0.5 delay:0.0 usingSpringWithDamping:0.5 initialSpringVelocity:10 options:UIViewAnimationOptionAllowUserInteraction animations:^{
+        self.dataPicker.frame = CGRectMake(0, HEIGHT / 2 + 90*MyHeight, WIDETH, HEIGHT / 2 - 80);
+        ok.frame = CGRectMake(WIDETH - 50, HEIGHT - 100*MyHeight, 40, 30);
+    } completion:^(BOOL finished) {
+        
+    }];
+}
+
+-(void)okClick
+{
+    [backView removeFromSuperview];
+    
+    [self.dataPicker removeFromSuperview];
+    
+    [ok removeFromSuperview];
+    if(dateStr == nil)
+    {
+        NSDate * localDate = [NSDate date];
+        //设置时间
+        NSDateFormatter * dateFormatt = [[NSDateFormatter alloc] init];
+        [dateFormatt setDateFormat:@"yyyy-MM-dd"];
+        dateStr = [dateFormatt stringFromDate:localDate];
+    }
+//    [record_btn setTitle:dateStr forState:UIControlStateSelected];
+//    record_btn.selected = YES;
+    self.extensionDateStr = dateStr;
+    
+    dateStr = nil;
+}
+
+-(void)dataChanged:(UIDatePicker *)sender
+{
+    NSDateFormatter * dateFormatt = [[NSDateFormatter alloc] init];
+    [dateFormatt setDateFormat:@"yyyy-MM-dd"];
+   dateStr = [dateFormatt stringFromDate:sender.date];
+    
+   }
+
 #pragma mark --- 全选点击事件
 -(void)marqueeBtnClck:(UIButton*)sender
 {
