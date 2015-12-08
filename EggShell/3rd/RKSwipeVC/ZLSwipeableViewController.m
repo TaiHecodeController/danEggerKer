bf9fa1e869675b777a069e74c331bfcee639fe37
rankilau <rankiliu@163.com>
2015-12-08 17:59:04 +0800

    判断左滑动右滑动

--- EggShell/3rd/RKSwipeVC/ZLSwipeableViewController.m
+++ EggShell/3rd/RKSwipeVC/ZLSwipeableViewController.m
@@ -14,6 +14,8 @@
 #import "AFAppRequest.h"
 #import "findJobModel.h"
 
+#define tabbarHeight 46
+
 @interface ZLSwipeableViewController ()
 {
      MBProgressHUD * _mbPro;
@@ -36,6 +38,9 @@
 @property (nonatomic,strong)AFRequestState * state;
 @property(nonatomic,assign)int page;
 
+//取消图片
+@property (nonatomic, strong) UIImageView *cancelView;
+
 @end
 
 @implementation ZLSwipeableViewController
@@ -46,8 +51,11 @@
     self.navigationController.toolbarHidden = NO;
     self.view.clipsToBounds = YES;
     self.view.backgroundColor = [UIColor whiteColor];
+    
+    self.cancelView.image = [UIImage imageNamed:@"不感兴趣"];
+    
 
-    // Do any additional setup after loading the view, typically from a nib.
+    //初始化职位数组，查询数据库
     self.jobArr = [[NSMutableArray alloc]init];
     _mbPro = [MBProgressHUD mbHubShowControllerView:self];
     self.page = 1;
@@ -77,6 +85,7 @@
         @"Asbestos"
     ];
 
+    
     self.reloadBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Reload"
                                                                 style:UIBarButtonItemStylePlain
                                                                target:self
@@ -120,29 +129,31 @@
         fixedSpace
     ];
 
+    //添加卡片视图
     ZLSwipeableView *swipeableView = [[ZLSwipeableView alloc] initWithFrame:CGRectZero];
     self.swipeableView = swipeableView;
+    self.swipeableView.allowedDirection = ZLSwipeableViewDirectionHorizontal;
+    
     [self.view addSubview:self.swipeableView];
-
+    //添加数据源代理，事件代理
     // Required Data Source
     self.swipeableView.dataSource = self;
-
     // Optional Delegate
     self.swipeableView.delegate = self;
-
     self.swipeableView.translatesAutoresizingMaskIntoConstraints = NO;
 
+    //autolayout自动布局,布局swipeableView
     NSDictionary *metrics = @{};
-
     [self.view addConstraints:[NSLayoutConstraint
-                                  constraintsWithVisualFormat:@"|-50-[swipeableView]-50-|"
+                                  constraintsWithVisualFormat:@"|-15-[swipeableView]-15-|"
                                                       options:0
                                                       metrics:metrics
                                                         views:NSDictionaryOfVariableBindings(
                                                                   swipeableView)]];
 
+    
     [self.view addConstraints:[NSLayoutConstraint
-                                  constraintsWithVisualFormat:@"V:|-120-[swipeableView]-100-|"
+                                  constraintsWithVisualFormat:@"V:|-20-[swipeableView]-66-|"
                                                       options:0
                                                       metrics:metrics
                                                         views:NSDictionaryOfVariableBindings(
@@ -160,7 +171,6 @@
         if (DataArr.count > 0)
         {
             NSLog(@"DataArr:%@",DataArr);
-//            [self.swipeableView discardAllViews];
             [self.jobArr removeAllObjects];
             [self.jobArr addObjectsFromArray:DataArr];
             if (self.page == 1)
@@ -169,13 +179,14 @@
             }
             else
             {
-                self.colorIndex = 1;
+                
+
+                //清除卡片，用新数据刷新卡片
                 [self.swipeableView discardAllViews];
                 [self.swipeableView loadViewsIfNeeded];
-//                [self nextViewForSwipeableView:self.swipeableView];
             }
             
-//
+
         }
         else
         {
@@ -234,38 +245,42 @@
 - (void)swipeableView:(ZLSwipeableView *)swipeableView
          didSwipeView:(UIView *)view
           inDirection:(ZLSwipeableViewDirection)direction {
-//    NSLog(@"did swipe in direction: %zd", direction);
+        NSLog(@"did swipe in direction: %zd", direction);
     NSLog(@"%ld",self.colorIndex);
     
     
 }
 
 - (void)swipeableView:(ZLSwipeableView *)swipeableView didCancelSwipe:(UIView *)view {
-//    NSLog(@"did cancel swipe");
+    NSLog(@"did cancel swipe");
 //     NSLog(@"%ld",self.colorIndex);
 }
 
 - (void)swipeableView:(ZLSwipeableView *)swipeableView
   didStartSwipingView:(UIView *)view
            atLocation:(CGPoint)location {
-//    NSLog(@"did start swiping at location: x %f, y %f", location.x, location.y);
+    NSLog(@"did start swiping at location: x %f, y %f", location.x, location.y);
 //     NSLog(@"%ld",self.colorIndex);
 //    //第8个开始滑动时，从新加载下一页数据
-//    if (self.colorIndex == 8)
-//    {
-//        self.colorIndex = 0;
-//        self.page ++;
-//        [self loadData:_mbPro page:self.page];
-//    }
-    
 }
 
 - (void)swipeableView:(ZLSwipeableView *)swipeableView
           swipingView:(UIView *)view
            atLocation:(CGPoint)location
           translation:(CGPoint)translation {
-//    NSLog(@"swiping at location: x %f, y %f, translation: x %f, y %f", location.x, location.y,
-//          translation.x, translation.y);
+    NSLog(@"swiping at location: x %f, y %f, translation: x %f, y %f", location.x, location.y,
+          translation.x, translation.y);
+    float oldX;
+    if (oldX >= location.x)
+    {
+        NSLog(@"左滑动");
+    }else
+    {
+        NSLog(@"右滑动");
+
+    }
+    oldX = location.x;
+    
 //     NSLog(@"%ld",self.colorIndex);
     
 }
@@ -273,7 +288,7 @@
 - (void)swipeableView:(ZLSwipeableView *)swipeableView
     didEndSwipingView:(UIView *)view
            atLocation:(CGPoint)location {
-//    NSLog(@"did end swiping at location: x %f, y %f", location.x, location.y);
+    NSLog(@"did end swiping at location: x %f, y %f", location.x, location.y);
 //     NSLog(@"%ld",self.colorIndex);
 //    NSLog(@"didEndSwipingView %ld",self.colorIndex);
     
@@ -288,11 +303,12 @@
     
     CardView *view = [[CardView alloc] initWithFrame:swipeableView.bounds];
     
-    //取出jobArr对应模型
+    //取出jobArr对应模型，对子控件进行赋值操作
     if (self.jobArr.count > 0 && self.colorIndex <= self.jobArr.count)
     {
         findJobModel * model = self.jobArr[self.colorIndex - 1];
         //添加一个标签试试
+        
         UILabel *lab = [[UILabel alloc]init];
         lab.frame = CGRectMake(0, 0, 100, 100);
         lab.textColor = [UIColor blackColor];
@@ -300,6 +316,7 @@
         [view addSubview:lab];
 
     }
+    
     //当colorIndex = 9时，走这个方法。大于8，每页的条数
     if (self.colorIndex > self.jobArr.count )
     {
@@ -312,36 +329,36 @@
     }
     else
     {
-//
+
     }
     
     view.backgroundColor = [self colorForName:self.colors[self.colorIndex]];
     self.colorIndex++;
 
-    if (self.loadCardFromXib) {
-        UIView *contentView =
-            [[NSBundle mainBundle] loadNibNamed:@"CardContentView" owner:self options:nil][0];
-        contentView.translatesAutoresizingMaskIntoConstraints = NO;
-        [view addSubview:contentView];
-
-        // This is important:
-        // https://github.com/zhxnlai/ZLSwipeableView/issues/9
-        NSDictionary *metrics =
-            @{ @"height" : @(view.bounds.size.height),
-               @"width" : @(view.bounds.size.width) };
-        NSDictionary *views = NSDictionaryOfVariableBindings(contentView);
-        [view addConstraints:[NSLayoutConstraint
-                                 constraintsWithVisualFormat:@"H:|[contentView(width)]"
-                                                     options:0
-                                                     metrics:metrics
-                                                       views:views]];
-        [view addConstraints:[NSLayoutConstraint
-                                 constraintsWithVisualFormat:@"V:|[contentView(height)]"
-                                                     options:0
-                                                     metrics:metrics
-                                                       views:views]];
-    }
-//    
+//    if (self.loadCardFromXib) {
+//        UIView *contentView =
+//            [[NSBundle mainBundle] loadNibNamed:@"CardContentView" owner:self options:nil][0];
+//        contentView.translatesAutoresizingMaskIntoConstraints = NO;
+////        [view addSubview:contentView];
+//
+//        // This is important:
+//        // https://github.com/zhxnlai/ZLSwipeableView/issues/9
+//        NSDictionary *metrics =
+//            @{ @"height" : @(view.bounds.size.height),
+//               @"width" : @(view.bounds.size.width) };
+//        NSDictionary *views = NSDictionaryOfVariableBindings(contentView);
+//        [view addConstraints:[NSLayoutConstraint
+//                                 constraintsWithVisualFormat:@"H:|[contentView(width)]"
+//                                                     options:0
+//                                                     metrics:metrics
+//                                                       views:views]];
+//        [view addConstraints:[NSLayoutConstraint
+//                                 constraintsWithVisualFormat:@"V:|[contentView(height)]"
+//                                                     options:0
+//                                                     metrics:metrics
+//                                                       views:views]];
+//    }
+//
     if (self.jobArr.count > 0)
     {
         return view;
