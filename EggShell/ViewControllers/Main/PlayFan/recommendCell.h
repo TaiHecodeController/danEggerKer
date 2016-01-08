aca9b00d7ca0177a04b7de8beebfbb2ef9cf5fff
rankilau <rankiliu@163.com>
2016-01-08 11:16:19 +0800

    爱活动推荐位cell

---
+++ EggShell/ViewControllers/Main/PlayFan/recommendCell.h
@@ -0,0 +1,24 @@
+//
+//  recommendCell.h
+//  EggShell
+//
+//  Created by 太和 on 16/1/8.
+//  Copyright © 2016年 wsd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface recommendCell : UITableViewCell
+
+@property (nonatomic, strong) UIImageView *banner;
+@property (nonatomic, strong) UILabel *titleLab;
+@property (nonatomic, strong) UIImageView *icon1;
+@property (nonatomic, strong) UIImageView *icon2;
+@property (nonatomic, strong) UIImageView *icon3;
+@property (nonatomic, strong) UIButton *collectionBtn;
+@property (nonatomic, strong) UIButton *joinBtn;
+@property (nonatomic, strong) UILabel *hostLab;
+@property (nonatomic, strong) UILabel *addressLab;
+@property (nonatomic, strong) UILabel *timeLab;
+
+@end
